4dc19e7350bf3a50702817f92ec67fbd3e921859
YangCheolJoo <rlaengks0001@naver.com>
2019-08-26 19:35:33 +0900

    Docs: Add encountered errors

--- SBDemo.java
+++ SBDemo.java
@@ -1,3 +1,4 @@
+// Demonstrate the small BASIC Interpreter.
 class SBDemo {
 	public static void main(String args[]) {
 		if (args.length != 1) {
@@ -12,3 +13,16 @@ class SBDemo {
 		}
 	}
 }
+
+/*
+ * errors...
+ *
+ * 1. 컴파일 오류(compile error)
+ * error while writing **: **.class (액세스가 거부되었습니다)
+ * javac -Xlint SBasic.java SBDemo.java
+ *
+ * 2. 실행 오류(execution error)
+ * 오류: 기본 클래스 **을(를) 찾거나 로드할 수 없습니다.
+ * java -classpath . SBDemo TEST.BAS
+ *
+ */
